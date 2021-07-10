1fc38b703aa83b3e68449e4527a8ed9781ed021c
arkbogda <ark.bogda@gmail.com>
2021-07-10 18:21:12 +0200

    Interpolation

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Lista Zadań';
+
+  get footer(): string {
+    return '2020 © Lista zadań,All rights reserved.';
+  }
+}
